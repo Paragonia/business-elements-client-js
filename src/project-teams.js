15bdb96daaba86ad7768848d89be70a179e57cfd
Dan Ghete <dan@product-foundry.com>
2016-07-08 16:23:58 +0300

    Added project team/teams API calls.

---
+++ src/project-teams.js
@@ -0,0 +1,73 @@
+"use strict";
+
+import endpoint from "./endpoint";
+import ProjectTeam from "./project-team";
+import * as requests from "./requests";
+
+/**
+ * Abstract representation of project team.
+ */
+export default class ProjectTeams {
+
+  /**
+   * Constructor.
+   *
+   * @param  {Tenant} tenant The tenant instance.
+   * @param  {Project} project The project instance.
+   */
+  constructor(tenant, project) {
+
+    /**
+     * The tenant.
+     * @type {Tenant}
+     */
+    this.tenant = tenant;
+
+    /**
+     * The project
+     * @type {Project}
+     */
+    this.project = project;
+  }
+
+  /**
+   * Retrieves the list of project teams for the current project.
+   *
+   * @param  {Object} options         The options object.
+   * @return {Promise<Array<Object>, Error>}
+   */
+  list(options = {}) {
+    return this.tenant.execute(requests.listProjectTeams(this.project.projectId), options)
+      .then((response) => {
+        if (response["_embedded"]) {
+          return response["_embedded"]["be:project_teams"];
+        } else {
+          return [];
+        }
+      });
+  }
+
+  /**
+   * Retrieve a project team object to perform operations on it.
+   *
+   * @param  {String} id The id of the project team.
+   * @return {ProjectTeam}
+   */
+  projectTeam(teamId) {
+    return new ProjectTeam(this.tenant, this.project, teamId);
+  }
+
+  /**
+   *  Create a new team for the current project
+   *
+   * @param {String} name       Context name
+   * @param {Object} options    The options object
+   * @returns {Promise.<Object, Error>}
+   */
+  create(teamId, permission, options = {}) {
+    return this.tenant.execute(
+      requests.createProjectTeam(this.project.projectId, teamId, permission),
+      options
+    );
+  }
+}
