308b91fefcddc6d17bd70d60c1d501ac63d84ce9
Andre Kampert <andre@product-foundry.com>
2017-03-17 02:24:05 +0100

    Add remaining SSE events

--- src/project-context-event.js
+++ src/project-context-event.js
@@ -47,11 +47,15 @@ export default class ProjectContextEvent {
   }
 }
 
+// Errors
+ProjectContextEvent.ERROR = new ProjectContextEvent("ERROR", "error");
+
 // Connect/recovered events
 ProjectContextEvent.CONNECTED = new ProjectContextEvent("CONNECTED", "connected");
+ProjectContextEvent.RECOVERING = new ProjectContextEvent("RECOVERING", "recovering");
 ProjectContextEvent.RECOVERED = new ProjectContextEvent("RECOVERED", "recovered");
 ProjectContextEvent.INITIALIZING = new ProjectContextEvent("INITIALIZING", "initializing");
-ProjectContextEvent.RECOVERING = new ProjectContextEvent("RECOVERING", "recovering");
+ProjectContextEvent.INITIALIZED = new ProjectContextEvent("INITIALIZED", "initialized");
 
 // Data events - values
 
