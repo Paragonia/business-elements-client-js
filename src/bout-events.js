ee184c4d1840ce9755dc2e82e715073703983bc4
Denny Leung <denny@product-foundry.com>
2017-09-12 14:06:47 +0200

    - fix bout-events to show on boutevents-list

--- src/bout-events.js
+++ src/bout-events.js
@@ -2,11 +2,12 @@
 
 import endpoint from "./endpoint";
 import BoutEvent from "./bout-event";
+import ResumableEventSource from "./resumable-event-source";
 
 /**
  * Abstract representation of a bout.
  */
-export default class BoutEvents {
+export default class BoutEvents extends ResumableEventSource {
 
   /**
    * Constructor.
@@ -14,6 +15,7 @@ export default class BoutEvents {
    * @param  {Tenant}  tenant     The tenant instance.
    */
   constructor(tenant) {
+    super(tenant.client.remote + endpoint("boutsEvents"));
 
     /**
      * The tenant.
@@ -22,34 +24,9 @@ export default class BoutEvents {
     this.tenant = tenant;
 
     /**
-     * The event source of bout updates
-     *
-     * The remote part of the url has to be explicitly appended, otherwise the event source will be creates on the same origin.
-     *
-     * @type {EventSource}
-     * */
-    this.eventSource = new EventSource(this.tenant.client.remote + endpoint("boutsEvents"), {withCredentials: true});
-
-    /**
      * Event types enum.
      * @type {BoutEvent}
      */
     this.type = BoutEvent;
-  }
-
-  /**
-   * Listen to the specified event.
-   * @param {BoutEvent} event to listen on.
-   * @param {BoutEvent.eventCallback} listener
-   */
-  on(event, listener) {
-    this.eventSource.addEventListener(event.bout, listener, false);
-  }
-
-  /**
-   * Closes the event source from the client.
-   */
-  close() {
-    this.eventSource.close();
   }
 }
